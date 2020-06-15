5812149fea022c5c7c888ca64644ed18d64e255d
Sophia Bell <sophbell87@gmail.com>
2020-06-15 17:18:41 +0100

    adding react test rendered

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+xtest('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders app title', () => {
+  const { getByText } = render(<App />);
+  const title = getByText("Todo List");
+  expect(title).toBeInTheDocument();
 });
